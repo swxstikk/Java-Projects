b66ea9a31118be3b71dd5f29e9a52239caf380ea
swastik <40032310+swxstikk@users.noreply.github.com>
2022-03-30 06:38:08 +1100

    final commit for case 1,2,3, and 4
    fixed all the bugs concerning cases 1 to 4 successfully. case 5 and 6 is being worked upon now.

--- src/Enrolment.java
+++ src/Enrolment.java
@@ -7,72 +7,39 @@ My email address: ss7796@uowmail.edu.au
 Assignment number: 1
 -------------------------------------------------------
 */
-
 public class Enrolment {
     private int number; //student number
     private String code, date; //subject code and enrolment date
-
-
     public Enrolment() {
-
         // creating a default empty constructor to avoid system to generate null values for the data members.
-
+        this(0, "","");
     }
-    public Enrolment(int numberInput, String codeInput) {
-
+    public Enrolment(int stdNumber) {
+        this.number=stdNumber;
+    }
+    public Enrolment(int numberInput, String codeInput, String dateInput) {
         // assigning values to data members
         this.number=numberInput;
         this.code=codeInput;
+        this.date=dateInput;
     }
-
     // defining the public access methods which returns value of private data members.
-
     public int getNumber() {
         return number;
     }
-
     public String getCode() {
         return code;
     }
-
-    public String getDate() {
-        return date;
-    }
-
+    public String getDate() {return date; }
     // defining public update methods that updates values of private data members.
-
     public void setNumber(int newNumber) {
         this.number=newNumber;
     }
-
-    public void setCode(String newCode) {
-        this.code=newCode;
-    }
-
-    public void setDate(String newDate) {
-        this.date=newDate;
-    }
-
+    public void setCode(String newCode) {this.code=newCode;}
+    public void setDate(String newDate) {this.date=newDate;}
     public String toString() {
         String e = "------------\n";
-
         e += "Enrolment details: " + number + code + date + "\n\n";
-
-        /*
-        'enrolments' arrayList is defined in another class(StudentEnrolmentSystem) Therefore, the error in the below lines of code.
-        Commenting out the for loop temporarily until I get a clarification from the lecturer regarding my question.
-            - Sent a mail to the lecturer on further clarity regarding the addition of data members and methods apart
-            from the ones mentioned in the assignment document.
-            - Test the toString override method in the StudentEnrolmentSystem class to check if the output is
-            properly formatted according to assignment specifications.
-
-        for(Enrolment enrolment:enrolments)
-            e+=enrolments;
-
-        return e+"------------";
-    }
-        */
         return e;
     }
-
 }
--- src/Student.java
+++ src/Student.java
@@ -7,23 +7,18 @@ My email address: ss7796@uowmail.edu.au
 Assignment number: 1
 -------------------------------------------------------
 */
-
 public class Student {
-
     private int number; //Student number
     private String name, dob, email, address, phone, degree; // essential student details
-
     public Student() {
-
         // creating a default empty constructor to avoid system to generate null values for the data members.
         this(0, "", "", "", "", "", "");
-
     }
-
+    public Student(int stdNumber) {
+        this.number=stdNumber;
+    }
     public Student(int studentNumber, String name, String dob, String email, String address, String phoneNumber, String degreeName) {
-
         // assigning values to all data members
-
         this.number = studentNumber;
         this.name = name;
         this.dob = dob;
@@ -32,86 +27,53 @@ public class Student {
         this.phone = phoneNumber;
         this.degree = degreeName;
     }
-
     // defining the public access methods which returns value of private data members.
-
     public int getNumber() {
         return number;
     }
-
     public String getName() {
         return name;
     }
-
     public String getDOB() {
         return dob;
     }
-
     public String getEmail() {
         return email;
     }
-
     public String getAddress() {
         return address;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public String getDegree() {
         return degree;
     }
-
     // defining public update methods that updates values of private data members.
-
     public void setNumber(int newNumber) {
         number = newNumber;
     }
-
     public void setName(String newName) {
         name = newName;
     }
-
     public void setDOB(String newDOB) {
         dob = newDOB;
     }
-
     public void setEmail(String newEmail) {
         email = newEmail;
     }
-
     public void setAddress(String newAddress) {
         address = newAddress;
     }
-
     public void setPhone(String newPhone) {
         phone = newPhone;
     }
-
     public void setDegree(String newDegree) {
         degree = newDegree;
     }
-
     public String toString() {
         String s= "";
-
         s += + number + ", " + name + ", " +dob +", " + email + ", " +address +", " + phone +", " + degree + "\n\n";
-
-        /*
-        'students' arrayList is defined in another class(StudentEnrolmentSystem) Therefore, the error in the below lines of code.
-        Commenting out the for loop temporarily until I get a clarification from the lecturer regarding my question.
-            - Sent a mail to the lecturer on further clarity regarding the addition of data members and methods apart
-            from the ones mentioned in the assignment document.
-            - Test the toString override method in the StudentEnrolmentSystem class to check if the output is
-            properly formatted according to assignment specifications.
-
-        for(Student student:students)
-            s+=students;
-
-        return s+"------------";
-    }
-        */
         return s;
     }
 }
